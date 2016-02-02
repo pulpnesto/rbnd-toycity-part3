46b578d8e34658b3564e96ebfb0eb7f956f5555a
pulpnesto <pulp.nesto@gmail.com>
2016-02-01 21:52:29 -0700

    Create Customer class with initialization, search by name and duplicate check/raise error.

---
+++ lib/customer.rb
@@ -0,0 +1,41 @@
+class Customer
+	attr_reader :name
+	@@customers = []
+
+	def initialize(options = {})
+		@name = options[:name]
+		add_customer
+	end
+
+	def self.all
+		@@customers
+	end
+
+	def self.find_by_name(name_to_search)
+		@@customers.each do |customer|
+			if customer.name == name_to_search
+				return customer
+			end
+		end
+	end
+
+	def duplicate?
+		check = []
+		check = @@customers.select{|customer|	customer.name == self.name}
+		if check.empty?
+			false
+		else
+			raise DuplicateProductError, "#{name} already exists."
+		end
+	end
+
+
+private
+
+		def add_customer
+			unless duplicate?
+				@@customers << self
+			end
+		end
+
+end
