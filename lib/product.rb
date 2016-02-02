d1413b28d4c7729f44e9ece8af1c674c48feb686
pulpnesto <pulp.nesto@gmail.com>
2016-02-01 21:22:12 -0700

    Create find_by_title method and throw err when duplicates detected

--- lib/product.rb
+++ lib/product.rb
@@ -9,12 +9,33 @@ class Product
 		add_product
 	end
 
-	def add_product
-		@@products << self
-	end
-
 	def self.all
 		@@products
 	end
 
+	def self.find_by_title(title_to_search)
+		@@products.each do |product|
+			if product.title == title_to_search
+				return product
+			end
+		end
+	end
+
+	def duplicate?
+		check = []
+		check = @@products.select{|product|	product.title == self.title}
+		if check.empty?
+			false
+		else
+			raise DuplicateProductError, "#{title} already exists."
+		end
+	end
+
+	private
+
+		def add_product
+			unless duplicate?
+				@@products << self
+			end
+		end
 end
